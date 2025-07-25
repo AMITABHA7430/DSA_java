c1c3afc13993d10d7daccb69aa4714737b0f8c25
AMITABHA7430 <amitabhag17@gmail.com>
2025-07-25 20:04:00 +0530

    Create recursion.java

---
+++ recursion.java
@@ -0,0 +1,19 @@
+//so this is the start of recurtion i am building my basic for DSA
+
+public class binarytwoD {
+
+    public static void main(String[] args) {
+      masg();
+//      masg1();//write a function that prints hallow world;
+    }
+
+    static void masg(){
+
+        System.out.printf("hello world\n  ");
+        masg();
+    }
+//    static void masg1(){
+//        System.out.printf("hello 1 world");
+//        masg();
+//    }
+}
