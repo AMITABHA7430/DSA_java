f295b600d104bd2fbb0c70fadb064c0e2f4c3fda
AMITABHA7430 <amitabhag17@gmail.com>
2025-08-01 21:44:04 +0530

    Create lowcomplexfebonacci.java

---
+++ lowcomplexfebonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static int fibonacci(int n) {
+        double sqrt5 = Math.sqrt(5);
+        double phi = (1 + sqrt5) / 2;
+        double psi = (1 - sqrt5) / 2;
+        
+        double fib = (Math.pow(phi, n) - Math.pow(psi, n)) / sqrt5;
+        return (int)Math.round(fib);  // Rounding for accuracy
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the nth term to get Fibonacci number:");
+        int a = scanner.nextInt();
+        int result = fibonacci(a);
+        System.out.println("Fibonacci number at position " + a + " is: " + result);
+    }
+}
