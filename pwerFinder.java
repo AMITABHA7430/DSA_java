ed27310e773b5d22a075419be9f867c5d9c0b8eb
AMITABHA7430 <amitabhag17@gmail.com>
2025-08-04 21:53:47 +0530

    Update pwerFinder.java

--- pwerFinder.java
+++ pwerFinder.java
@@ -1,1 +1,27 @@
 
+import java.util.Scanner;
+
+public class magic {
+
+    public static void main(String[] args) {
+        Scanner ami = new Scanner(System.in); 
+
+      int base;
+      base= ami.nextInt();
+
+       int power;
+       power = ami.nextInt();
+        int ans=1;
+       while (power > 0){
+           if((power & 1)==1){
+               ans *= base ;
+
+           }
+           base*=base;
+           power = power>> 1;
+       }
+        System.out.println(ans);
+
+    }
+}
+
