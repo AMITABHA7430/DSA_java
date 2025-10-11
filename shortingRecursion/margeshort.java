7d79eb55e1a0483648d029ca1641883b84797fae
Amitabha Ghosh <amitabhag17@gmail.com>
2025-10-11 07:03:24 +0530

    Create margeshort.java

---
+++ shortingRecursion/margeshort.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+class Main {
+   
+    static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    static int[] merge(int[] left, int[] right) {
+        int[] mix = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                mix[k++] = left[i++];
+            } else {
+                mix[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            mix[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            mix[k++] = right[j++];
+        }
+
+        return mix;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+
+        System.out.println("Before sorting:");
+        System.out.println(Arrays.toString(arr));
+
+        arr = mergeSort(arr);
+
+        System.out.println("After sorting:");
+        System.out.println(Arrays.toString(arr));
+    }
+}
