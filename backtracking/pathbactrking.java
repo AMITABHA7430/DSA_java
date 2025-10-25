3725db823fe84935e0f8ecb3716fe670a565d968
Amitabha Ghosh <amitabhag17@gmail.com>
2025-10-25 08:30:47 +0530

    Create pathbactrking.java

---
+++ backtracking/pathbactrking.java
@@ -0,0 +1,23 @@
+public class Dice {
+    public static void main(String[] args) {
+        dBacktracking("", 3, 3);
+    }
+
+    static void dBacktracking(String p, int r, int c) {
+        // Base case: reached destination
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+
+        // Move Right if possible (c > 1)
+        if (c > 1) {
+            dBacktracking(p + 'R', r, c - 1);
+        }
+
+        // Move Down if possible (r > 1)
+        if (r > 1) {
+            dBacktracking(p + 'D', r - 1, c);
+        }
+    }
+}
